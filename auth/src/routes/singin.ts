4113883bedd532724ebc6e473d4d5bbee90834a3
water <water34545@yandex.ru>
2023-01-02 21:25:42 +0700

    add validateRequest middleware

--- auth/src/routes/singin.ts
+++ auth/src/routes/singin.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import {validateRequest} from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/singin', (req, res) => {
-  res.send('Hi there');
-});
+router.post(
+  '/api/users/singin',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Provide a valid E-mail!'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply the password!')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+
+    res.send('Hi there');
+  }
+);
 
 export { router as singInRouter };
--- auth/src/routes/singup.ts
+++ auth/src/routes/singup.ts
@@ -1,48 +1,47 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from  'express-validator';
+import { body } from  'express-validator';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user';
-import { RequestValidationError } from  '../errors/requestValidationError';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/singup', [
-  body('email')
-    .isEmail()
-    .withMessage('Provide a valid E-mail!'),
-  body('password')
-    .trim()
-    .isLength({ min: 6, max: 20})
-    .withMessage('Password must been between 4 and 20 characters!')
-], async (req: Request, res: Response) => {
-  const errors = validationResult(req);
+router.post(
+  '/api/users/singup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Provide a valid E-mail!'),
+    body('password')
+      .trim()
+      .isLength({ min: 6, max: 20})
+      .withMessage('Password must been between 4 and 20 characters!')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
 
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError('Email already in use!');
+    }
+
+    const user = User.build({email, password});
+    await user.save();
+
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, process.env.JWT_KEY!);
+
+    req.session = {
+      jwt: userJwt
+    };
+
+    res.status(201).send(user);
   }
-
-  const { email, password } = req.body;
-
-  const existingUser = await User.findOne({ email });
-
-  if (existingUser) {
-    throw new BadRequestError('Email already in use!');
-  }
-
-  const user = User.build({email, password});
-  await user.save();
-
-  const userJwt = jwt.sign({
-    id: user.id,
-    email: user.email
-  }, process.env.JWT_KEY!);
-
-  req.session = {
-    jwt: userJwt
-  };
-
-  res.status(201).send(user);
-});
+);
 
 export { router as singUpRouter };
