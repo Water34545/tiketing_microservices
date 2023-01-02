ede0b32352df7cb90b286c705cb657aa136c554e
water <water34545@yandex.ru>
2023-01-02 22:02:02 +0700

    add currentUser middleware

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from "express";
-import {CustomError} from "../errors/custom-error";
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction ) => {
   console.log('Something went wrong:', err);
