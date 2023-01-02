875394f73e2b582bfbcb395a138c073558f4288c
water <water34545@yandex.ru>
2023-01-02 21:34:31 +0700

    add singin logic

--- auth/src/routes/singin.ts
+++ auth/src/routes/singin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import {validateRequest} from '../middlewares/validate-request';
+import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import {BadRequestError} from '../errors/bad-request-error';
+import {Password} from '../services/password';
 
 const router = express.Router();
 
@@ -17,8 +21,33 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const {email, password} = req.body;
 
-    res.send('Hi there');
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials!');
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid credentials!');
+    }
+
+    const userJwt = jwt.sign({
+      id: existingUser.id,
+      email: existingUser.email
+    }, process.env.JWT_KEY!);
+
+    req.session = {
+      jwt: userJwt
+    };
+
+    res.status(201).send(existingUser);
   }
 );
 
