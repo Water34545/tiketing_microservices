5acfa60da6666de8359fe6c3e791b4f8c00912ca
water <water34545@yandex.ru>
2023-01-09 21:16:32 +0700

    adds commin errors and middlewares

---
+++ common/src/index.ts
@@ -0,0 +1,11 @@
+export * from './errors/databaseConnectionError';
+export * from './errors/requestValidationError';
+export * from './errors/custom-error';
+export * from './errors/bad-request-error';
+export * from './errors/not-authorised-error';
+export * from './errors/not-found-error';
+
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
