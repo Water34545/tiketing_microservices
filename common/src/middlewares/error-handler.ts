1a9fe61c0fef68f54067adb4cdc2673c9c1bcb7e
water <water34545@yandex.ru>
2023-01-10 15:17:26 +0700

    updates

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -7,7 +7,8 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
     return res.status(err.statusCode).send({errors: err.serializeErrors()});
   }
 
-  res.status(500).send({
+  console.error(err);
+  res.status(400).send({
     errors: [{ massage: 'Something went wrong' }]
   });
 }
